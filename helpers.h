2b5d21a6ec05d97e24e15925e9723106ce8d77eb
swiecki <daniel.swiecki@gmail.com>
2012-10-06 15:06:45 -0400

    added some linked list helper functions

--- helpers.h
+++ helpers.h
@@ -5,3 +5,5 @@ char ***tokenify2 (char **s, const char *sep);
 void freeAll1(char **array);
 void freeAll2(char ***array);
 char **readFile(char *filename);
+void listInsert(pid_t pid, char *command, int state, struct node **head);
+void listClear(struct node *list);
