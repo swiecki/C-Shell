dd64b01cc6495cd438bab2dba77343eeb6529e19
swiecki <daniel.swiecki@gmail.com>
2012-10-06 15:19:28 -0400

    fixed bugs in linked list helpers

--- helpers.c
+++ helpers.c
@@ -168,21 +168,20 @@ char **readFile(char *filename){
 	return toreturn;
 }
 
-struct node {
-	pid_t pid;
-	char command[1024];
-	int state;
-	struct node *next;
-};
-
 void listInsert(pid_t pid, char *command, int state, struct node **head){
 	struct node *newnode = malloc(sizeof(struct node));
 	strncpy(newnode->command, command, 1024);
 	newnode->pid = pid;
 	newnode->state = state;
-	newnode->head = *head;
+	newnode->next = *head;
 	*head = newnode;
 }
+
+/*void listDelete(pid_t pid, struct node *list){
+	while (list != NULL) {
+			
+	}
+}*/
 
 void listClear(struct node *list) {
 	while (list != NULL) {
--- helpers.h
+++ helpers.h
@@ -1,3 +1,4 @@
+#include <sys/types.h>
 void removeWhitespace(char *string);
 void removeComment(char *string);
 char **tokenify (char *s, const char *sep);
@@ -5,5 +6,14 @@ char ***tokenify2 (char **s, const char *sep);
 void freeAll1(char **array);
 void freeAll2(char ***array);
 char **readFile(char *filename);
+
+
+struct node {
+	pid_t pid;
+	char command[1024];
+	int state;
+	struct node *next;
+};
+
 void listInsert(pid_t pid, char *command, int state, struct node **head);
 void listClear(struct node *list);
